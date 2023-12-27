6c699e16a0aa2e302a17bb36d16b3dd3345dd6b5
AmpaLampaX <aprotr01@fesb.hr>
2023-12-27 19:56:08 +0100

    admin stuff

--- models/Register.php
+++ models/Register.php
@@ -45,7 +45,12 @@ class Register extends \yii\db\ActiveRecord implements IdentityInterface
     /**
      * {@inheritdoc}
      */
-   
+    public $isAdmin;
+    public function getIsAdmin()
+    {
+        return $this->isAdmin == 1; 
+    }
+    
 //hash, authkey and accessToken
      public function beforeSave($insert)
      {
