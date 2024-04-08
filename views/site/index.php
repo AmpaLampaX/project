59c00cef61da2903d6f72d29f64435ade3d59eb5
AmpaLampaX <aprotr01@fesb.hr>
2024-04-08 21:09:27 +0200

    editing home page

--- views/site/index.php
+++ views/site/index.php
@@ -9,45 +9,42 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Go erasmus!</h1>
+        <h1 class="display-4">GO ERASMUS!</h1>
 
-        <p class="lead">Let's learn more about students opportunities </p>
+        <p class="lead">Unlock the World, Unleash Your Future </p>
 
-        <p><a class="btn btn-lg btn-success" href="https://www.unist.hr/me%C4%91unarodna-suradnja/me%C4%91unarodna-razmjena-i-natjecaji/natjecaji/mobilnost-studenata">Sign up for it</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://www.unist.hr/me%C4%91unarodna-suradnja/me%C4%91unarodna-razmjena-i-natjecaji/natjecaji/mobilnost-studenata">Begin Your Quest</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+                <h2>Start Your Journey</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Unlock the full potential of your Erasmus journey by registering with us. This is your first step towards exploring a world of opportunities and connecting with like-minded individuals. By signing up, 
+                    you gain access to exclusive resources, personalized advice, and a community of prospective and former Erasmus students eager to share their experiences. Registration is quick, easy, and your gateway 
+                    to an enriching Erasmus exchange. Don't wait to discover what's possible – join our community today and start shaping your future.</p>
 
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/"> Join the Erasmus Elite &raquo;</a></p>
             </div>
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+                <h2>Explore Your Horizons</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Discover the gateway to your Erasmus adventure through our University Opportunities section. Here, a comprehensive list of faculties from our university awaits, each offering unique Erasmus exchange 
+                    opportunities abroad. Whether you dream of studying in a specific country or are exploring the academic offerings of different institutions, our intuitive search feature assists you in finding exactly 
+                    what you need. Delve into the possibilities and take the first step toward an unforgettable educational experience. Click below to start exploring Erasmus opportunities tailored to your interests.</p>
 
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Adventure Launcher &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Dive Into Discussions</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Embark on your Erasmus journey with insights from those who've walked the path before you. Our Forum is a vibrant community space where prospective and former Erasmus students share experiences, 
+                    tips, and advice. Whether you're looking for accommodation, seeking the perfect roommate, or curious about which university to choose for your Erasmus exchange, our forum is your go-to resource. 
+                    Dive into discussions, ask questions, and get a real feel for your upcoming adventure. Ready to connect and learn? Click the button below to explore our Forum.</p>
 
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-outline-secondary" href= "http://localhost/index.php?r=article%2Findex" >Engage & Exchange &raquo;</a></p>
             </div>
         </div>
 
