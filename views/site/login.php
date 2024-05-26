ebb5aa8903be7f7674e7f4f632882f1f8081ad26
AmpaLampaX <aprotr01@fesb.hr>
2024-05-26 16:43:32 +0200

    Login design

--- views/site/login.php
+++ views/site/login.php
@@ -2,7 +2,6 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
-
 /** @var app\models\LoginForm $model */
 
 use yii\bootstrap5\ActiveForm;
@@ -10,6 +9,56 @@ use yii\bootstrap5\Html;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerCss("
+    .site-login {
+        background-color: #4793AF;
+        color: #FFFFFF;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .site-login h1 {
+        color: #8B322C;
+        font-weight: bold;
+    }
+    .site-login p {
+        color: #FFFFFF;
+    }
+    .site-login .form-group .btn-primary {
+        background-color: #8B322C;
+        border-color: #8B322C;
+    }
+    .site-login .form-group .btn-primary:hover {
+        background-color: #DD5746;
+        border-color: #DD5746;
+    }
+    .form-control {
+        border-radius: 5px;
+        background-color: #FFFFFF;
+        color: #000000;
+    }
+    .form-control:focus {
+        border-color: #DD5746;
+        box-shadow: 0 0 0 0.2rem rgba(221, 87, 70, 0.25);
+    }
+    .custom-checkbox .custom-control-input:checked ~ .custom-control-label::before {
+        background-color: #FFC470;
+        border-color: #FFC470;
+    }
+    .custom-control-label {
+        color: #FFC470;
+        font-weight: bold;
+    }
+    .invalid-feedback {
+        color: #FFC470;
+    }
+    label[for=\"loginform-username\"], 
+    label[for=\"loginform-password\"] {
+        color: #8B322C;
+        font-weight: bold;
+    }
+");
+
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -34,7 +83,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= $form->field($model, 'password')->passwordInput() ?>
 
             <?= $form->field($model, 'rememberMe')->checkbox([
-                'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                'template' => "<div class=\"custom-control custom-checkbox\">{input} <label class=\"custom-control-label\" for=\"loginform-rememberme\" style=\"color: #FFC470; font-weight: bold;\">Remember Me</label></div>\n<div class=\"col-lg-8\">{error}</div>",
             ]) ?>
 
             <div class="form-group">
